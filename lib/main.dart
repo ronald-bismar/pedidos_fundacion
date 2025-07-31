5473d41fba951c77bd47bb989efc7b40bd8c37fb
Ronald <ronald.bismark003@gmail.com>
2025-07-30 23:22:30 -0400

    Colocando el providerScope y subiendo el minSDK

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pedidos_fundacion/features/authentication/presentation/screens/auth_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const MyApp());
+  runApp(ProviderScope(child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
