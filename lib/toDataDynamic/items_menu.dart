7f448da68df6f4ebeddda65014c0bf9a681c691b
Ronald <ronald.bismark003@gmail.com>
2025-09-10 21:40:51 -0400

    Modificando el modo ver del beneficiario

--- lib/toDataDynamic/items_menu.dart
+++ lib/toDataDynamic/items_menu.dart
@@ -4,31 +4,31 @@ import 'package:pedidos_fundacion/presentation/screen_factory.dart';
 
 final menuItems = [
   ItemMenu(title: 'Pedidos', icon: Icons.list, screenType: ScreenType.pedidos),
-  ItemMenu(title: 'Lugares', icon: Icons.place, screenType: ScreenType.lugares),
-  ItemMenu(title: 'Grupos', icon: Icons.group, screenType: ScreenType.grupos),
-  ItemMenu(
+    ItemMenu(
     title: 'Entregas',
     icon: Icons.delivery_dining,
     screenType: ScreenType.entregas,
   ),
+    ItemMenu(
+    title: 'Beneficiarios',
+    icon: Icons.group,
+    screenType: ScreenType.beneficiarios,
+  ),
+    ItemMenu(
+    title: 'Asistencia Beneficiarios',
+    icon: Icons.group_add,
+    screenType: ScreenType.asistenciaBeneficiarios,
+  ),
+  ItemMenu(title: 'Grupos', icon: Icons.group, screenType: ScreenType.grupos),
   ItemMenu(
     title: 'Personal',
     icon: Icons.person,
     screenType: ScreenType.personal,
   ),
   ItemMenu(
-    title: 'Asistencia Beneficiarios',
-    icon: Icons.group_add,
-    screenType: ScreenType.asistenciaBeneficiarios,
-  ),
-  ItemMenu(
-    title: 'Beneficiarios',
-    icon: Icons.group,
-    screenType: ScreenType.beneficiarios,
-  ),
-  ItemMenu(
     title: 'Reportes',
     icon: Icons.report,
     screenType: ScreenType.reportes,
   ),
+  ItemMenu(title: 'Lugares', icon: Icons.place, screenType: ScreenType.lugares),
 ];
